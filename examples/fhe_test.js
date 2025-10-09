6725f29d018b6148f7c2b4d6bb8bd7d341034bd9
Emirhan Alper Semerci <157852959+Alperrsm@users.noreply.github.com>
2025-10-09 18:22:10 +0300

    add simple FHE simulation JavaScript script

---
+++ examples/fhe_test.js
@@ -0,0 +1,8 @@
+// fhe_test.js
+// Simple JS script for Zama Developer Program contribution
+
+function simulateFHE(a, b) {
+    return a + b; // simulate encrypted addition
+}
+
+console.log("Simulated FHE result:", simulateFHE(20, 25));
